44e6620d6c4f335176553562620aeac19f3cdad9
Aditya21326 <80681858+Aditya21326@users.noreply.github.com>
2021-06-02 11:14:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function adduser()
+{
+    var player1=document.getElementById("player1_name_input").value;
+
+var player2=document.getElementById("player2_name_input").value;
+
+localStorage.setItem("player1", player1);
+localStorage.setItem("player2", player2)
+}
+
+function send()
+{
+
+}
+
